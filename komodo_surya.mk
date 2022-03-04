6ffadc6cabd121dc1c03ebbc219f99b390e3ec05
buglessx <muhammad.065117057@unpak.ac.id>
2022-03-04 15:16:34 +0000

    surya: intial bringup komodo
    Signed-off-by: buglessx <muhammad.065117057@unpak.ac.id>

--- komodo_surya.mk
+++ komodo_surya.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Nusantara stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+$(call inherit-product, vendor/komodo/config/common.mk)
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -27,29 +27,23 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # Blur
 TARGET_USES_BLUR := true
 
-# GApps
-TARGET_GAPPS_ARCH := arm64
-USE_GAPPS := true
-
-# Lawnchair
-USE_LAWNCHAIR := true
-
-# Official
-NAD_BUILD_TYPE := OFFICIAL
-
 # Pixel Charging
 USE_PIXEL_CHARGING := true
 
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-PRODUCT_NAME := nad_surya
+PRODUCT_NAME := komodo_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+KOMODO_OFFICIAL := true
+KOMODO_GAPPS_TYPE := gapps
+KOMODO_VARIANT := RELEASE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="raven-user 12 SD1A.210817.036 7805805 release-keys"
